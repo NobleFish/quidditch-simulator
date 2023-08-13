090544e550267d5ab6d48d6c27ddabb195221e82
NobleFish <sirnoblefish@gmail.com>
2023-08-13 19:00:40 -0400

    Added some failing TeamTests

--- test/TeamTest.cpp
+++ test/TeamTest.cpp
@@ -1,48 +1,47 @@
 
 // Include files for the test
 #include <gtest/gtest.h>
+#include <Team.hpp>
+#include <Player.hpp>
 
 namespace teamTest {
 // The fixture for testing class Foo.
-class TeamTest : public ::testing::Test {
-    /*
-    protected:
-    // You can remove any or all of the following functions if their bodies would
-    // be empty.
-
-    
-    TeamTest() {
-        // You can do set-up work for each test here.
-    }
-
-    ~TeamTest() override {
-        // You can do clean-up work that doesn't throw exceptions here.
-    }
-
-    // If the constructor and destructor are not enough for setting up
-    // and cleaning up each test, you can define the following methods:
-
-    void SetUp() override {
-        // Code here will be called immediately after the constructor (right
-        // before each test).
-    }
-
-    void TearDown() override {
-        // Code here will be called immediately after each test (right
-        // before the destructor).
-    }
-    */
-};
-
+class TeamTest : public ::testing::Test {};
 
 TEST_F(TeamTest, Initialization)
 {    
-    // Tests
+    Team t1("Team1");
+    EXPECT_EQ(t1.getName(),"Team1");
+    EXPECT_TRUE(t1.getRoster().empty());
 }
 
 TEST_F(TeamTest, ManualUpdates)
 {    
-    // Tests
+    Team t1("Team1");
+    t1.setName("ModTeam1");
+    EXPECT_EQ(t1.getName(),"ModTeam1");
+
 }
+
+TEST_F(TeamTest, AddToRoster)
+{    
+    Team t1("Team1");
+
+    // Need to change this :: Player instance vs. Player
+    Player p1("Player1",{0,0}, 0.4F, 0.4F);
+
+    t1.addToRoster(&p1);
+    EXPECT_TRUE(t1.onRoster(&p1));
+
+}
+
+TEST_F(TeamTest, TeamValidity){   
+    EXPECT_EQ(true,false);
+}
+
+TEST_F(TeamTest, StartingLineUp){
+    EXPECT_EQ(true,false);
+}
+
 
 }  // namespace
